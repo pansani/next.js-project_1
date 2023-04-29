d06189346076a56976406bc868158df9167e6256
Joao Claudio Mendonca Pansani <pansanijoaoclaudio@gmail.com>
2023-04-28 21:25:38 -0300

    mudei o estilo do header

--- components/SectionOne.tsx
+++ components/SectionOne.tsx
@@ -1,50 +1,80 @@
-import React from 'react';
-import Image from 'next/image';
-import logoHeader from './../public/assets/logo_place_header.png'
-import NavHeader from './NavBar';
+import React from "react";
+import Image from "next/image";
+import logoHeader from "./../public/assets/logo_place_header.png";
+import NavHeader from "./NavBar";
+import headerImage from "./../public/assets/header-image.png"
 
 export default function Header() {
   return (
-    <div style={{
-      backgroundColor: '#F2F2F2',
-      minHeight: '600px',
-      maxHeight: '756px',
-      overflow: 'hidden',
-      position: 'relative',
-      display: 'flex',
-      alignItems: 'center',
-    }}>
-      <NavHeader></NavHeader>
-      <Image 
-      src={logoHeader}
-      alt="Logo Practicum" 
-      width={200}
-      height={50}
+    <div
       style={{
-        position: 'absolute',
-        left: '64px',
-        top: '30px',
+        backgroundColor: "#F2F2F2",
+        minHeight: "600px",
+        maxHeight: "756px",
+        overflow: "hidden",
+        position: "relative",
+        display: "flex",
+        alignItems: "center",
       }}
+    >
+      <NavHeader></NavHeader>
+      <Image
+        src={logoHeader}
+        alt="Logo Practicum"
+        width={200}
+        height={50}
+        style={{
+          position: "absolute",
+          left: "64px",
+          top: "30px",
+        }}
       />
-      <h1 style={{
-        width: '730px',
-        lineHeight: '96px',
-        fontSize: '102px',
-        fontWeight: 'bold',
-        marginLeft: '64px',
-        alignSelf: 'center'
-        
-      }}> 
+      <h1
+        style={{
+          width: "730px",
+          lineHeight: "96px",
+          fontSize: "102px",
+          fontWeight: "bold",
+          marginLeft: "64px",
+          alignSelf: "center",
+        }}
+      >
         Aprendendo a Aprender
       </h1>
-      <p style={{
-        lineHeight: '25',
-        fontSize: '18',
-        fontWeight: 'normal'
-      }}>
-      As técnicas de aprendizado mais eficazes que
-      você pode aplicar ao seu cotidiano
+      <p
+        style={{
+          lineHeight: "25px",
+          fontSize: "18px",
+          fontWeight: "normal",
+          position: "absolute",
+          bottom: "30px",
+          left: "64px",
+        }}
+      >
+        As técnicas de aprendizado mais eficazes que você pode aplicar ao seu
+        cotidiano
+        <a
+          style={{
+            textDecoration: "none",
+            color: "#2F80ED",
+            display: "block",
+          }}
+        >
+          Saiba Mais➝
+        </a>
       </p>
+      <Image
+        src={headerImage}
+        alt="Homem refletindo ao topo de uma montanha"
+        width={756}
+        height={608}
+        style={{
+          position: 'absolute',
+          right: '0px',
+          bottom: '0px',
+          zIndex: '1'
+        }}
+      ></Image>
     </div>
   );
 }
