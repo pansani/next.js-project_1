0125ae7e355b8f3190e341c6f7551ef58ae114c6
Joao Claudio Mendonca Pansani <pansanijoaoclaudio@gmail.com>
2023-04-30 16:33:15 -0300

    almost finished feynman component

--- components/FeynmanSection.tsx
+++ components/FeynmanSection.tsx
@@ -1,9 +1,44 @@
 import React from "react";
+import feynmanImage from "./../public/assets/feynman.png";
 
-export default function Feynman(){
-    return(
-    <div>
-        <h1>A tecnica de Feynman</h1>
+export default function Feynman() {
+  return (
+    <div
+      style={{
+        position: "relative",
+        height: "790px",
+        display: "flex",
+        justifyContent: "center",
+        backgroundImage: "url(/assets/feynman.png)",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "left bottom",
+        backgroundSize: "867px 637px",
+        marginBottom: "5px",
+        backgroundColor: "#F2F2F2",
+      }}
+    >
+      <h1
+        style={{
+          width: "775px",
+          lineHeight: 1.15,
+          fontSize: "120px",
+          fontWeight: "bold",
+          textAlign: "center",
+          position: "absolute",
+          zIndex: 1,
+        }}
+      >
+        A tecnica de Feynman
+      </h1>
+      <h2 style={{
+        lineHeight: '51px',
+        fontSize: "36px",
+        fontWeight: "normal",
+        textAlign: "center",
+        position: "absolute",
+        zIndex: 1,
+        top: '45%',
+      }}>Aprender sem esquecer</h2>
     </div>
-)
+  );
 }
